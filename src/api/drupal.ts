36c324040d66f85ef78e14344977a5681e3df99b
Adon <a@atriskmedia.com>
2024-03-08 22:25:25 -0500

    we got drupal; we got tractstack types

--- src/api/drupal.ts
+++ src/api/drupal.ts
@@ -1,7 +1,21 @@
 import { Jsona } from "jsona";
 import { DrupalJsonApiParams } from "drupal-jsonapi-params";
-import type { DrupalFile, DrupalNode } from "../types.ts";
-import type { TJsonApiBody } from "jsona/lib/JsonaTypes";
+import type {
+  DrupalFile,
+  Markdown,
+  MarkdownDatum,
+  Pane,
+  PaneDatum,
+  StoryFragment,
+  StoryFragmentDatum,
+  TractStack,
+  TractStackDatum,
+  Menu,
+  MenuDatum,
+  Resource,
+  ResourceDatum,
+} from "../types.ts";
+import type { TJsonApiBody, TJsonaModel } from "jsona/lib/JsonaTypes";
 
 interface DrupalApiBody extends TJsonApiBody {
   /* eslint-disable @typescript-eslint/no-explicit-any */
@@ -11,38 +25,212 @@ interface DrupalApiBody extends TJsonApiBody {
 export const baseUrl: string = import.meta.env.DRUPAL_BASE_URL;
 
 export const fetchUrl = async (url: string): Promise<any> => {
-  /* eslint-disable @typescript-eslint/no-explicit-any */
-  const data: any = [];
+  let data: TJsonaModel = [];
   let more = true;
   while (more) {
     const request: Response = await fetch(url);
     const json: string | DrupalApiBody = await request.json();
     const dataFormatter: Jsona = new Jsona();
-    const thisData: any = dataFormatter.deserialize(json);
-    data.push(...thisData);
+    const thisData = dataFormatter.deserialize(json);
+    if (thisData.length) data = data.concat(thisData);
+    else data.push(thisData);
     url = typeof json !== `string` ? json?.links?.next?.href : null;
     if (typeof url === `undefined`) more = false;
   }
   return data;
 };
 
-export const getFiles = async (): Promise<DrupalFile[]> => {
+export const getAllFiles = async (): Promise<DrupalFile[]> => {
   const params: DrupalJsonApiParams = new DrupalJsonApiParams();
   params.addFields("file--file", ["type", "id"]).addFilter("status", "1");
   const path: string = params.getQueryString();
-
   return await fetchUrl(baseUrl + "/jsonapi/file/file?" + path);
 };
 
-export const getAllStoryFragments = async (): Promise<DrupalNode[]> => {
+export const getStoryFragment = async (
+  id: string
+): Promise<StoryFragmentDatum[]> => {
   const params: DrupalJsonApiParams = new DrupalJsonApiParams();
   params
-    .addFields("node--story_fragment", ["type", "id", "title"])
+    .addFields("node--story_fragment", [
+      "type",
+      "id",
+      "drupal_internal__nid",
+      "title",
+      "field_social_image_path",
+      "field_tailwind_background_colour",
+      "field_tract_stack",
+      "field_panes",
+      "field_menu",
+    ])
     .addFilter("status", "1");
   const path: string = params.getQueryString();
+  return await fetchUrl(
+    baseUrl + "/jsonapi/node/story_fragment/" + id + "?" + path
+  );
+};
 
-  console.log(baseUrl + "/jsonapi/node/story_fragment?" + path);
+export const getAllStoryFragments = async (): Promise<StoryFragment[]> => {
+  const params: DrupalJsonApiParams = new DrupalJsonApiParams();
+  params
+    .addFields("node--story_fragment", ["type", "id", "title", "field_slug"])
+    .addFilter("status", "1");
+  const path: string = params.getQueryString();
   return await fetchUrl(baseUrl + "/jsonapi/node/story_fragment?" + path);
+};
+
+export const getTractStack = async (id: string): Promise<TractStackDatum[]> => {
+  const params: DrupalJsonApiParams = new DrupalJsonApiParams();
+  params
+    .addFields("node--tractstack", [
+      "type",
+      "id",
+      "drupal_internal__nid",
+      "title",
+      "field_social_image_path",
+      "field_story_fragments",
+    ])
+    .addFilter("status", "1");
+  const path: string = params.getQueryString();
+  return await fetchUrl(
+    baseUrl + "/jsonapi/node/tractstack/" + id + "?" + path
+  );
+};
+
+export const getAllTractStacks = async (): Promise<TractStack[]> => {
+  const params: DrupalJsonApiParams = new DrupalJsonApiParams();
+  params
+    .addFields("node--tractstack", ["type", "id", "title", "field_slug"])
+    .addFilter("status", "1");
+  const path: string = params.getQueryString();
+  return await fetchUrl(baseUrl + "/jsonapi/node/tractstack?" + path);
+};
+
+export const getPane = async (id: string): Promise<PaneDatum[]> => {
+  const params: DrupalJsonApiParams = new DrupalJsonApiParams();
+  params
+    .addFields("node--pane", [
+      "type",
+      "id",
+      "drupal_internal__nid",
+      "title",
+      "field_markdown",
+      "field_image",
+      "field_image_svg",
+    ])
+    .addFields("node--markdown", [
+      "type",
+      "id",
+      "drupal_internal__nid",
+      "title",
+      "field_markdown_body",
+      "field_image",
+      "field_image_svg",
+    ])
+    .addInclude(["field_markdown"])
+    .addFilter("status", "1");
+  const path: string = params.getQueryString();
+  return await fetchUrl(baseUrl + "/jsonapi/node/pane/" + id + "?" + path);
+};
+
+export const getAllPanes = async (): Promise<Pane[]> => {
+  const params: DrupalJsonApiParams = new DrupalJsonApiParams();
+  params
+    .addFields("node--pane", ["type", "id", "title", "field_slug"])
+    .addFilter("status", "1");
+  const path: string = params.getQueryString();
+  return await fetchUrl(baseUrl + "/jsonapi/node/pane?" + path);
+};
+
+export const getMarkdown = async (id: string): Promise<MarkdownDatum[]> => {
+  const params: DrupalJsonApiParams = new DrupalJsonApiParams();
+  params
+    .addFields("node--markdown", [
+      "type",
+      "id",
+      "drupal_internal__nid",
+      "title",
+      "field_markdown_body",
+      "field_image",
+      "field_image_svg",
+    ])
+    .addFilter("status", "1");
+  const path: string = params.getQueryString();
+  return await fetchUrl(baseUrl + "/jsonapi/node/markdown/" + id + "?" + path);
+};
+
+export const getAllMarkdowns = async (): Promise<Markdown[]> => {
+  const params: DrupalJsonApiParams = new DrupalJsonApiParams();
+  params
+    .addFields("node--markdown", [
+      "type",
+      "id",
+      "title",
+      "field_slug",
+      "field_category_slug",
+    ])
+    .addFilter("status", "1");
+  const path: string = params.getQueryString();
+  return await fetchUrl(baseUrl + "/jsonapi/node/markdown?" + path);
+};
+
+export const getResource = async (id: string): Promise<ResourceDatum[]> => {
+  const params: DrupalJsonApiParams = new DrupalJsonApiParams();
+  params
+    .addFields("node--resource", [
+      "type",
+      "id",
+      "drupal_internal__nid",
+      "title",
+      "field_slug",
+      "field_category_slug",
+      "field_options",
+      "field_action_lisp",
+      "field_oneliner",
+    ])
+    .addFilter("status", "1");
+  const path: string = params.getQueryString();
+  return await fetchUrl(baseUrl + "/jsonapi/node/resource/" + id + "?" + path);
+};
+
+export const getAllResources = async (): Promise<Resource[]> => {
+  const params: DrupalJsonApiParams = new DrupalJsonApiParams();
+  params
+    .addFields("node--resource", [
+      "type",
+      "id",
+      "title",
+      "field_slug",
+      "field_category_slug",
+    ])
+    .addFilter("status", "1");
+  const path: string = params.getQueryString();
+  return await fetchUrl(baseUrl + "/jsonapi/node/resource?" + path);
+};
+
+export const getMenu = async (id: string): Promise<MenuDatum[]> => {
+  const params: DrupalJsonApiParams = new DrupalJsonApiParams();
+  params
+    .addFields("node--menu", [
+      "type",
+      "id",
+      "drupal_internal__nid",
+      "title",
+      "field_theme",
+      "field_options",
+    ])
+    .addFilter("status", "1");
+  const path: string = params.getQueryString();
+  return await fetchUrl(baseUrl + "/jsonapi/node/menu/" + id + "?" + path);
+};
+
+export const getAllMenus = async (): Promise<Menu[]> => {
+  const params: DrupalJsonApiParams = new DrupalJsonApiParams();
+  params
+    .addFields("node--menu", ["type", "id", "title", "field_theme"])
+    .addFilter("status", "1");
+  const path: string = params.getQueryString();
+  return await fetchUrl(baseUrl + "/jsonapi/node/menu?" + path);
 };
 
 export default fetchUrl;
--- src/types.ts
+++ src/types.ts
@@ -66,3 +66,82 @@ export interface DrupalFileMeta extends Record<string, any> {
   width: number;
   height: number;
 }
+
+interface FileNode {
+  type: `file--file`;
+  id: string;
+}
+interface MarkdownNode {
+  type: `node--markdown`;
+  id: string;
+}
+
+// Tract Stack types
+
+interface T8kNode {
+  type: string;
+  id: string;
+  title: string;
+  links: {
+    self: {
+      href: string;
+    };
+  };
+}
+export interface StoryFragment extends T8kNode {
+  field_slug: string;
+}
+export interface TractStack extends T8kNode {
+  field_slug: string;
+}
+export interface Pane extends T8kNode {
+  field_slug: string;
+}
+export interface Markdown extends T8kNode {
+  field_slug: string;
+  field_category_slug: string | null;
+}
+export interface Resource extends T8kNode {
+  field_slug: string;
+  field_category_slug: string | null;
+}
+export interface Menu extends T8kNode {
+  field_theme: string;
+}
+
+export interface StoryFragmentDatum extends StoryFragment {
+  drupal_internal__nid: string;
+}
+export interface TractStackDatum extends TractStack {
+  drupal_internal__nid: string;
+}
+export interface PaneDatum extends Pane {
+  drupal_internal__nid: string;
+  field_options: string;
+  field_is_context_pane: boolean;
+  field_height_ratio_desktop: string;
+  field_height_ratio_tablet: string;
+  field_height_ratio_mobile: string;
+  field_height_offset_desktop: number;
+  field_height_offset_tablet: number;
+  field_height_offset_mobile: number;
+  field_markdown: MarkdownNode[];
+  field_image: FileNode[];
+  field_image_svg: FileNode[];
+}
+export interface MarkdownDatum extends Markdown {
+  drupal_internal__nid: string;
+  field_markdown_body: string;
+  field_image: FileNode[];
+  field_image_svg: FileNode[];
+}
+export interface ResourceDatum extends Resource {
+  drupal_internal__nid: string;
+  field_options: string;
+  field_action_lisp: string;
+  field_oneliner: string;
+}
+export interface MenuDatum extends Menu {
+  drupal_internal__nid: string;
+  field_options: string;
+}
