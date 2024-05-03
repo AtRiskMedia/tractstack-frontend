b2174fc50a4da4d78156c1f6cc50d56f21ed0b38
Adon <a@atriskmedia.com>
2024-05-03 00:29:59 -0400

    fixed ogImage; tuned up on beliefs

--- src/components/widgets/Belief.tsx
+++ src/components/widgets/Belief.tsx
@@ -1,10 +1,6 @@
 import { Fragment, useState, useEffect } from "react";
 import { Listbox, Transition } from "@headlessui/react";
-import {
-  XMarkIcon,
-  CheckIcon,
-  ChevronUpDownIcon,
-} from "@heroicons/react/20/solid";
+import { CheckIcon, ChevronUpDownIcon } from "@heroicons/react/20/solid";
 import { useStore } from "@nanostores/react";
 import { heldBeliefsScales, heldBeliefsTitles } from "@assets/beliefs";
 import { classNames } from "../../utils/helpers";
@@ -37,7 +33,6 @@ export const Belief = ({
   };
   const thisScale = [start, ...thisScaleRaw];
   const [selected, setSelected] = useState(start);
-  const [hasSelect, setHasSelect] = useState(false);
 
   useEffect(() => {
     const hasMatchingBelief = $heldBeliefsAll
@@ -49,20 +44,9 @@ export const Belief = ({
             .filter((e: BeliefOptionDatum) => e.slug === hasMatchingBelief.verb)
             .at(0)
         : false;
-    if (knownOffset && knownOffset?.slug) {
-      setSelected(knownOffset);
-      setHasSelect(true);
-    } else {
-      setSelected(start);
-      setHasSelect(false);
-    }
+    if (knownOffset && knownOffset?.slug) setSelected(knownOffset);
+    else setSelected(start);
   }, [$heldBeliefsAll]);
-
-  const handleReset = () => {
-    setSelected(start);
-    handleClick(start);
-    setHasSelect(false);
-  };
 
   const handleClick = (e: BeliefOptionDatum) => {
     if (e.id > 0) {
@@ -108,119 +92,103 @@ export const Belief = ({
   return (
     <>
       {extra ? <span className="mr-2">{extra}</span> : null}
-      {hasSelect ? (
-        <div className="mt-6 text-black text-sm font-main font-normal">
-          You selected: <span className="font-bold">{selected.name}</span>
-          <span className="ml-2">
-            <button onClick={() => handleReset()}>
-              <XMarkIcon
-                className="h-5 w-5 text-mylightgrey hover:text-myblue"
-                aria-hidden="true"
-              />
-            </button>
-          </span>
-        </div>
-      ) : (
-        <div className="block mt-3 w-fit">
-          <Listbox value={selected} onChange={handleClick}>
-            {({ open }) => (
-              <>
-                <div className="z-90 relative mt-1">
-                  <Listbox.Button
-                    className={classNames(
-                      selected?.color
-                        ? `border-${selected.color.substring(3)}`
-                        : `bg-slate-200`,
-                      `relative w-full cursor-default rounded-md border bg-white text-black py-2 pl-3 pr-10 text-left shadow-sm focus:border-myorange focus:outline-none focus:ring-1 focus:ring-myorange`
-                    )}
-                  >
-                    <span className="flex items-center">
-                      <span
-                        aria-label="Color swatch for belief"
-                        className={classNames(
-                          `motion-safe:animate-pulse`,
-                          selected?.color ? selected.color : `bg-myorange`,
-                          `inline-block h-2 w-2 flex-shrink-0 rounded-full`
-                        )}
-                      />
-                      <span className="ml-3 block truncate">
-                        {selected?.name || thisTitle}
-                      </span>
+      <div className="block mt-3 w-fit">
+        <Listbox value={selected} onChange={handleClick}>
+          {({ open }) => (
+            <>
+              <div className="z-90 relative mt-1">
+                <Listbox.Button
+                  className={classNames(
+                    selected?.color
+                      ? `border-${selected.color.substring(3)}`
+                      : `bg-slate-200`,
+                    `relative w-full cursor-default rounded-md border bg-white text-black py-2 pl-3 pr-10 text-left shadow-sm focus:border-myorange focus:outline-none focus:ring-1 focus:ring-myorange`
+                  )}
+                >
+                  <span className="flex items-center">
+                    <span
+                      aria-label="Color swatch for belief"
+                      className={classNames(
+                        `motion-safe:animate-pulse`,
+                        selected?.color ? selected.color : `bg-myorange`,
+                        `inline-block h-2 w-2 flex-shrink-0 rounded-full`
+                      )}
+                    />
+                    <span className="ml-3 block truncate">
+                      {selected?.name || thisTitle}
                     </span>
-                    <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
-                      <ChevronUpDownIcon
-                        className="h-5 w-5 text-mylightgrey"
-                        aria-hidden="true"
-                      />
-                    </span>
-                  </Listbox.Button>
+                  </span>
+                  <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+                    <ChevronUpDownIcon
+                      className="h-5 w-5 text-mylightgrey"
+                      aria-hidden="true"
+                    />
+                  </span>
+                </Listbox.Button>
 
-                  <Transition
-                    show={open}
-                    as={Fragment}
-                    leave="transition ease-in duration-100"
-                    leaveFrom="opacity-100"
-                    leaveTo="opacity-0"
-                  >
-                    <Listbox.Options className="absolute mt-1 max-h-60 overflow-auto rounded-md bg-white py-1 text-sm shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                      {thisScale.map((factor: BeliefOptionDatum) => (
-                        <Listbox.Option
-                          key={factor.id}
-                          className={({ active }) =>
-                            classNames(
-                              active
-                                ? `text-myblue bg-slate-200`
-                                : `text-black`,
-                              `relative cursor-default select-none py-2 pl-3 pr-9`
-                            )
-                          }
-                          value={factor}
-                        >
-                          {({ selected, active }) => (
-                            <>
-                              <div className="flex items-center">
-                                <span
-                                  className={classNames(
-                                    factor.color,
-                                    `inline-block h-2 w-2 flex-shrink-0 rounded-full`
-                                  )}
+                <Transition
+                  show={open}
+                  as={Fragment}
+                  leave="transition ease-in duration-100"
+                  leaveFrom="opacity-100"
+                  leaveTo="opacity-0"
+                >
+                  <Listbox.Options className="absolute mt-1 max-h-60 overflow-auto rounded-md bg-white py-1 text-sm shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                    {thisScale.map((factor: BeliefOptionDatum) => (
+                      <Listbox.Option
+                        key={factor.id}
+                        className={({ active }) =>
+                          classNames(
+                            active ? `text-myblue bg-slate-200` : `text-black`,
+                            `relative cursor-default select-none py-2 pl-3 pr-9`
+                          )
+                        }
+                        value={factor}
+                      >
+                        {({ selected, active }) => (
+                          <>
+                            <div className="flex items-center">
+                              <span
+                                className={classNames(
+                                  factor.color,
+                                  `inline-block h-2 w-2 flex-shrink-0 rounded-full`
+                                )}
+                                aria-hidden="true"
+                              />
+                              <span
+                                className={classNames(
+                                  selected ? `underline` : ``,
+                                  `ml-3 block truncate`
+                                )}
+                              >
+                                {factor.name}
+                              </span>
+                            </div>
+
+                            {selected ? (
+                              <span
+                                className={classNames(
+                                  active ? `text-white` : `text-black`,
+                                  `absolute inset-y-0 right-0 flex items-center px-2`
+                                )}
+                              >
+                                <CheckIcon
+                                  className="h-5 w-5"
                                   aria-hidden="true"
                                 />
-                                <span
-                                  className={classNames(
-                                    selected ? `underline` : ``,
-                                    `ml-3 block truncate`
-                                  )}
-                                >
-                                  {factor.name}
-                                </span>
-                              </div>
-
-                              {selected ? (
-                                <span
-                                  className={classNames(
-                                    active ? `text-white` : `text-black`,
-                                    `absolute inset-y-0 right-0 flex items-center px-2`
-                                  )}
-                                >
-                                  <CheckIcon
-                                    className="h-5 w-5"
-                                    aria-hidden="true"
-                                  />
-                                </span>
-                              ) : null}
-                            </>
-                          )}
-                        </Listbox.Option>
-                      ))}
-                    </Listbox.Options>
-                  </Transition>
-                </div>
-              </>
-            )}
-          </Listbox>
-        </div>
-      )}
+                              </span>
+                            ) : null}
+                          </>
+                        )}
+                      </Listbox.Option>
+                    ))}
+                  </Listbox.Options>
+                </Transition>
+              </div>
+            </>
+          )}
+        </Listbox>
+      </div>
     </>
   );
 };
