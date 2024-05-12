05ef4d597fef63c51097dde86aa61643a4bea28a
Adon <a@atriskmedia.com>
2024-05-12 01:11:35 -0400

    added wordmark mode

--- src/components/other/StorySteps.tsx
+++ src/components/other/StorySteps.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useStore } from "@nanostores/react";
-import { sync,auth } from "../../store/auth";
+import { sync, auth } from "../../store/auth";
 import { MapIcon } from "@heroicons/react/24/outline";
 
 export const StorySteps = () => {
--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,5 @@
 export const CONCIERGE_SYNC_INTERVAL = 4000;
-export const THRESHOLD_READ = 42000;
-export const THRESHOLD_GLOSSED = 7000;
+export const THRESHOLD_READ =import.meta.env.PUBLIC_THRESHOLD_READ  || 42000;
+export const THRESHOLD_GLOSSED = import.meta.env.PUBLIC_THRESHOLD_GLOSSED || 7000;
 export const JWT_LIFETIME = 15 * 60 * 1000;
+export const WORDMARK_MODE = import.meta.env.PUBLIC_WORDMARK_MODE || "default";
