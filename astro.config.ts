ec09e4f0a67aed8b1b98434e0b63f01b41dc14c7
Adon <a@atriskmedia.com>
2024-05-20 22:21:24 -0400

    added partytown for google analytics

--- astro.config.ts
+++ astro.config.ts
@@ -4,6 +4,7 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import { SITE } from "./src/config";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
@@ -23,6 +24,11 @@ export default defineConfig({
   integrations: [
     tailwind(),
     react(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
     sitemap({
       filter: page =>
         !page.includes(`/breadcrumbs/`) &&
